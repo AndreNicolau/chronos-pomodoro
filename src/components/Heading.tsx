af948cd93ca29ab59888704740e1b1077a5ee2c2
André Nicolau <andre.filipe.nicolau29@gmail.com>
2025-09-11 22:18:01 +0100

    Aula 18

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,7 +1,9 @@
 import styles from './Heading.module.css';
 
-export function Heading() {
-  console.log(styles);
+type HeadingProps = {
+  children: React.ReactNode;
+};
 
-  return <h1 className={styles.heading}>Olá Mundo!</h1>;
+export function Heading({ children }: HeadingProps) {
+  return <h1 className={styles.heading}>{children}</h1>;
 }
