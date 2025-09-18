1fa61410bdb95aba1775670e025bd8004af7afb7
André Nicolau <andre.filipe.nicolau29@gmail.com>
2025-09-18 20:46:55 +0100

    Aula 24

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,21 @@
+import { HistoryIcon, HouseIcon, Settings2Icon, SunIcon } from 'lucide-react';
+import styles from './styles.module.css';
+
+export function Menu() {
+  return (
+    <nav className={styles.menu}>
+      <a className={styles.menuLink} href='#'>
+        <HouseIcon size={64} />
+      </a>
+      <a className={styles.menuLink} href='#'>
+        <HistoryIcon size={64} />
+      </a>
+      <a className={styles.menuLink} href='#'>
+        <Settings2Icon size={64} />
+      </a>
+      <a className={styles.menuLink} href='#'>
+        <SunIcon size={64} />
+      </a>
+    </nav>
+  );
+}
