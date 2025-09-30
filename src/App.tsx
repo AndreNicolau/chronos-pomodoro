2ca33aa16850d8f4e3caa963ff8780327232b654
André Nicolau <andre.filipe.nicolau29@gmail.com>
2025-09-30 19:26:45 +0100

    Aula 35

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,19 @@
+import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
+import { Footer } from './components/Footer';
+import { Cycles } from './components/Cycles';
 import { PlayCircleIcon } from 'lucide-react';
 import { Container } from './components/Container';
 import { CountDown } from './components/CountDown';
-import { Cycles } from './components/Cycles';
-import { DefaultButton } from './components/DefaultButton';
 import { DefaultInput } from './components/DefaultInput';
-import { Logo } from './components/Logo';
-import { Menu } from './components/Menu';
+import { DefaultButton } from './components/DefaultButton';
 
 import './styles/global.css';
 import './styles/theme.css';
-import { Footer } from './components/Footer';
-import { Heading } from './components/Heading';
-import { useState } from 'react';
 
 export function App() {
-  const [numero, setNumero] = useState(0);
-
-  function handleClick() {
-    setNumero(prevState => prevState + 1);
-  }
-
   return (
     <>
-      <Heading>Numero: {numero}</Heading>
-      <button onClick={handleClick}>Aumenta</button>
-
       <Container>
         <Logo />
       </Container>
@@ -43,7 +32,7 @@ export function App() {
             <DefaultInput
               type='text'
               id='meuInput'
-              labelText={numero.toString()}
+              labelText='Task'
               placeholder='Insira algo'
             />
           </div>
