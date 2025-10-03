7d2558d54dcd067462b12b40f9cad6428a8e5078
André Nicolau <andre.filipe.nicolau29@gmail.com>
2025-10-03 22:19:59 +0100

    Aula 47

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Home } from './pages/Home';
 import './styles/global.css';
 import './styles/theme.css';
 import type { TaskStateModel } from './models/TaskStateModel';
+import { TaskContext } from './contexts/TaskContext';
 
 const initialState: TaskStateModel = {
   tasks: [],
@@ -21,5 +22,9 @@ const initialState: TaskStateModel = {
 export function App() {
   const [state, setState] = useState(initialState);
 
-  return <Home />;
+  return (
+    <TaskContext.Provider value={{ outraChave: '1234' }}>
+      <Home />;
+    </TaskContext.Provider>
+  );
 }
