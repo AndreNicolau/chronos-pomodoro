abb826ac58362af9b6aedf669aa52d45db1969b9
André Nicolau <andre.filipe.nicolau29@gmail.com>
2025-10-04 18:46:49 +0100

    Aula 50

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,25 +2,10 @@ import { DefaultButton } from '../DefaultButton';
 import { Cycles } from '../Cycles';
 import { PlayCircleIcon } from 'lucide-react';
 import { DefaultInput } from '../DefaultInput';
-import { useTaskContext } from '../../contexts/TaskContext';
 
 export function MainForm() {
-  const { setState } = useTaskContext();
-
-  function handleClick() {
-    setState((prevState) => {
-      return {
-        ...prevState,
-        formattedSecondsRemaining: '21:00',
-      };
-    });
-  }
-
   return (
     <form className='form' action=''>
-      <button onClick={handleClick} type='button'>
-        Clicar
-      </button>
       <div className='formRow'>
         <DefaultInput
           type='text'
